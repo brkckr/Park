082ad5d82750682a68f973d1609dc3f8670828f2
brkckr <burakcakir1991@gmail.com>
2022-06-17 18:57:57 +0300

    change theme.

--- app/src/main/java/com/github/brkckr/park/ui/theme/Color.kt
+++ app/src/main/java/com/github/brkckr/park/ui/theme/Color.kt
@@ -2,7 +2,13 @@ package com.github.brkckr.park.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val MapLight = Color(0xFF29446B)
+val MapDark = Color(0xFF192330)
+
+val Logo1 = Color(0xFF80DEEA)
+val Logo2 = Color(0xFF00BCD4)
+val Logo3 = Color(0xFF00ACC1)
+val Logo4 = Color(0xFF00838F)
+
+val White = Color(0xFFFFFFFF)
+val Black = Color(0xFF000000)
