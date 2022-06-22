fb8c55f97296ccaf9dec89e8d73bc000ec5066db
brkckr <burakcakir1991@gmail.com>
2022-06-22 18:36:03 +0300

    add map style.

---
+++ app/src/main/java/com/github/brkckr/park/util/MapStyle.kt
@@ -0,0 +1,144 @@
+package com.github.brkckr.park.util
+
+object MapStyle {
+    const val json = "\n" +
+            "\n" +
+            "[\n" +
+            "    {\n" +
+            "        \"featureType\": \"all\",\n" +
+            "        \"elementType\": \"geometry\",\n" +
+            "        \"stylers\": [\n" +
+            "            {\n" +
+            "                \"color\": \"#202c3e\"\n" +
+            "            }\n" +
+            "        ]\n" +
+            "    },\n" +
+            "    {\n" +
+            "        \"featureType\": \"all\",\n" +
+            "        \"elementType\": \"labels.text.fill\",\n" +
+            "        \"stylers\": [\n" +
+            "            {\n" +
+            "                \"gamma\": 0.01\n" +
+            "            },\n" +
+            "            {\n" +
+            "                \"lightness\": 20\n" +
+            "            },\n" +
+            "            {\n" +
+            "                \"weight\": \"1.39\"\n" +
+            "            },\n" +
+            "            {\n" +
+            "                \"color\": \"#ffffff\"\n" +
+            "            }\n" +
+            "        ]\n" +
+            "    },\n" +
+            "    {\n" +
+            "        \"featureType\": \"all\",\n" +
+            "        \"elementType\": \"labels.text.stroke\",\n" +
+            "        \"stylers\": [\n" +
+            "            {\n" +
+            "                \"weight\": \"0.96\"\n" +
+            "            },\n" +
+            "            {\n" +
+            "                \"saturation\": \"9\"\n" +
+            "            },\n" +
+            "            {\n" +
+            "                \"visibility\": \"on\"\n" +
+            "            },\n" +
+            "            {\n" +
+            "                \"color\": \"#000000\"\n" +
+            "            }\n" +
+            "        ]\n" +
+            "    },\n" +
+            "    {\n" +
+            "        \"featureType\": \"all\",\n" +
+            "        \"elementType\": \"labels.icon\",\n" +
+            "        \"stylers\": [\n" +
+            "            {\n" +
+            "                \"visibility\": \"off\"\n" +
+            "            }\n" +
+            "        ]\n" +
+            "    },\n" +
+            "    {\n" +
+            "        \"featureType\": \"landscape\",\n" +
+            "        \"elementType\": \"geometry\",\n" +
+            "        \"stylers\": [\n" +
+            "            {\n" +
+            "                \"lightness\": 30\n" +
+            "            },\n" +
+            "            {\n" +
+            "                \"saturation\": \"9\"\n" +
+            "            },\n" +
+            "            {\n" +
+            "                \"color\": \"#29446b\"\n" +
+            "            }\n" +
+            "        ]\n" +
+            "    },\n" +
+            "    {\n" +
+            "        \"featureType\": \"poi\",\n" +
+            "        \"elementType\": \"geometry\",\n" +
+            "        \"stylers\": [\n" +
+            "            {\n" +
+            "                \"saturation\": 20\n" +
+            "            }\n" +
+            "        ]\n" +
+            "    },\n" +
+            "    {\n" +
+            "        \"featureType\": \"poi.park\",\n" +
+            "        \"elementType\": \"geometry\",\n" +
+            "        \"stylers\": [\n" +
+            "            {\n" +
+            "                \"lightness\": 20\n" +
+            "            },\n" +
+            "            {\n" +
+            "                \"saturation\": -20\n" +
+            "            }\n" +
+            "        ]\n" +
+            "    },\n" +
+            "    {\n" +
+            "        \"featureType\": \"road\",\n" +
+            "        \"elementType\": \"geometry\",\n" +
+            "        \"stylers\": [\n" +
+            "            {\n" +
+            "                \"lightness\": 10\n" +
+            "            },\n" +
+            "            {\n" +
+            "                \"saturation\": -30\n" +
+            "            }\n" +
+            "        ]\n" +
+            "    },\n" +
+            "    {\n" +
+            "        \"featureType\": \"road\",\n" +
+            "        \"elementType\": \"geometry.fill\",\n" +
+            "        \"stylers\": [\n" +
+            "            {\n" +
+            "                \"color\": \"#193a55\"\n" +
+            "            }\n" +
+            "        ]\n" +
+            "    },\n" +
+            "    {\n" +
+            "        \"featureType\": \"road\",\n" +
+            "        \"elementType\": \"geometry.stroke\",\n" +
+            "        \"stylers\": [\n" +
+            "            {\n" +
+            "                \"saturation\": 25\n" +
+            "            },\n" +
+            "            {\n" +
+            "                \"lightness\": 25\n" +
+            "            },\n" +
+            "            {\n" +
+            "                \"weight\": \"0.01\"\n" +
+            "            }\n" +
+            "        ]\n" +
+            "    },\n" +
+            "    {\n" +
+            "        \"featureType\": \"water\",\n" +
+            "        \"elementType\": \"all\",\n" +
+            "        \"stylers\": [\n" +
+            "            {\n" +
+            "                \"lightness\": -20\n" +
+            "            }\n" +
+            "        ]\n" +
+            "    }\n" +
+            "]\n" +
+            "\n"
+}
