d8340f68187aff837414ede38117afbc956b01ff
brkckr <burakcakir1991@gmail.com>
2022-06-30 00:57:00 +0300

    add navigation

--- app/src/main/java/com/github/brkckr/park/MainActivity.kt
+++ app/src/main/java/com/github/brkckr/park/MainActivity.kt
@@ -3,36 +3,57 @@ package com.github.brkckr.park
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.animation.ExperimentalAnimationApi
+import com.github.brkckr.park.ui.navigation.ParkNavigation
 import com.github.brkckr.park.ui.theme.ParkTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    @OptIn(ExperimentalAnimationApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ParkTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+                ParkNavigation()
             }
         }
     }
 }
 
+/*@OptIn(ExperimentalAnimationApi::class)
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun Navigation() {
+    val navController = rememberAnimatedNavController()
+    AnimatedNavHost(navController = navController, startDestination = Screen.ParkMapScreen.route) {
+        composable(route = Screen.ParkMapScreen.route) {
+            ParkMapScreen(navController)
+        }
+        composable(
+            route = Screen.ParkDetailScreen.route + "/{id}",
+            arguments = listOf(
+                navArgument("id") {
+                    type = NavType.IntType
+                }
+            )
+        ) { entry ->
+            ParkDetailScreen(id = entry.arguments?.getInt("id"))
+        }
+    }
+
 }
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ParkTheme {
-        Greeting("Android")
+sealed class Screen(val route: String) {
+    object ParkMapScreen : Screen("park_map_screen")
+    object ParkDetailScreen : Screen("park_detail_screen")
+
+    fun withArgs(vararg args: Int): String {
+        return buildString {
+            append(route)
+            args.forEach { arg ->
+                append("/$arg")
+            }
+        }
     }
-}
+}*/
